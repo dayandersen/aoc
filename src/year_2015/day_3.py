c963ee26ac0c12e78f5d0a1b53f78400f0da0204
dandersen <dayandersen@gmail.com>
2024-10-28 21:46:21 -0700

    Day 3 complete

--- src/year_2015/day_3.py
+++ src/year_2015/day_3.py
@@ -2,34 +2,48 @@ from solver import AdventSolver
 
 
 class Day3Solver(AdventSolver):
-    def part_1(self, input_data: list[str]) -> str:
-        # Implement day 1 solution
-        return "(42, 123)"
-
     """
-    The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. 
-    Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to 
-    the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+    Moves are always exactly one house to the north (^), south (v), east (>), or west (<).
+    After each move, he delivers another present to the house at his new location.
+    However, the elf back at the north pole has had a little too much eggnog, and so his directions are a little off,
+    and Santa ends up visiting some houses more than once. How many houses receive at least one present?
 
     For example:
-        1. A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present 
-        plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
 
-        2. A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present 
-        plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+        > delivers presents to 2 houses: one at the starting location, and one to the east.
+        ^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
+        ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
 
     """
 
-    def part_2(self, input_data: list[str]) -> str:
-        total = 0
-        for dimensions_str in input_data:
-            dimensions = [
-                int(dimension) if dimension.isdigit() else 0
-                for dimension in dimensions_str.split("x")
-            ]
-            if len(dimensions) == 3:
-                total += dimensions[0] * dimensions[1] * dimensions[2]
-                dimensions_sorted = sorted(dimensions)
-                total += dimensions_sorted[0] * 2 + dimensions_sorted[1] * 2
+    def part_1(self, input_data: list[str]) -> str:
+        coords = (0, 0)
+        visited = set()
+        visited.add(coords)
+        dirs = {"^": (0, 1), "v": (0, -1), ">": (1, 0), "<": (-1, 0)}
+        for direction in input_data[0]:
+            change = dirs[direction] if direction in dirs else (0, 0)
+            coords = (coords[0] + change[0], coords[1] + change[1])
+            visited.add(coords)
+        return str(len(visited))
 
-        return str(total)
+    def part_2(self, input_data: list[str]) -> str:
+        coords_1 = (0, 0)
+        coords_2 = (0, 0)
+        visited = set()
+        visited.add(coords_1)
+        flip_flop = False
+        dirs = {"^": (0, 1), "v": (0, -1), ">": (1, 0), "<": (-1, 0)}
+        for direction in input_data[0]:
+            change = dirs[direction] if direction in dirs else (0, 0)
+
+            if flip_flop == False:
+                coords_1 = (coords_1[0] + change[0], coords_1[1] + change[1])
+                visited.add(coords_1)
+            else:
+                coords_2 = (coords_2[0] + change[0], coords_2[1] + change[1])
+                visited.add(coords_2)
+
+            flip_flop = not flip_flop
+
+        return str(len(visited))
